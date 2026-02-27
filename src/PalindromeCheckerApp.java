40cca6adf2c5c007eb19a62450a0ca128b10ac3e
kishore904 <kishorekumarkishorekumar7381@gmail.com>
2026-02-27 15:23:57 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,28 +4,16 @@ public class PalindromeCheckerApp {
 
 
         // --------------------------------------
-// UC6: Numeric Palindrome Check
+// UC1: Welcome message
 // --------------------------------------
-
-        java.util.Scanner scanner4 = new java.util.Scanner(System.in);
-
-        System.out.print("UC6: Enter a number to check: ");
-        int number = scanner4.nextInt();
-        scanner4.nextLine(); // Consume newline
-
-        int temp = number;
-        int reverse = 0;
-
-        while (temp != 0) {
-            int digit = temp % 10;
-            reverse = reverse * 10 + digit;
-            temp = temp / 10;
-        }
-
-        if (reverse == number) {
-            System.out.println("Result: " + number + " is a Numeric Palindrome\n");
-        } else {
-            System.out.println("Result: " + number + " is NOT a Numeric Palindrome\n");
-        }
+        System.out.println("======================================");
+        System.out.println("     PALINDROME CHECKER APPLICATION    ");
+        System.out.println("======================================");
+        System.out.println("Version : 1.0");
+        System.out.println("Author  : Kishore Kumar");
+        System.out.println("--------------------------------------");
+        System.out.println("This application checks if a string is");
+        System.out.println("a palindrome. Let's begin!");
+        System.out.println("--------------------------------------\n");
     }
 }
