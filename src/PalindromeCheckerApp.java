4501b04142a4681b25ae2d3b23e01114a347fb35
kishore904 <kishorekumarkishorekumar7381@gmail.com>
2026-02-27 14:46:48 +0530

    UC2: Add hardcoded palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,31 @@
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
 
+        // UC1: Welcome message
+        System.out.println("======================================");
+        System.out.println("     PALINDROME CHECKER APPLICATION    ");
+        System.out.println("======================================");
+        System.out.println("Version : 1.0");
+        System.out.println("Author  : Kishore Kumar");
+        System.out.println("--------------------------------------");
+        System.out.println("This application checks if a string is");
+        System.out.println("a palindrome. Let's begin!");
+        System.out.println("--------------------------------------\n");
+
+        // --------------------------------------
+        // UC2: Hardcoded palindrome check (PUT HERE)
+        // --------------------------------------
+        String word = "MADAM";
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        System.out.println("UC2: Checking hardcoded word...");
+        System.out.println("Input Word: " + word);
+
+        if (word.equalsIgnoreCase(reversed)) {
+            System.out.println("Result: " + word + " is a Palindrome\n");
+        } else {
+            System.out.println("Result: " + word + " is NOT a Palindrome\n");
+        }
     }
 }
